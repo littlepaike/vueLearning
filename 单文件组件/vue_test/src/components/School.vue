c4212424c46ff04e85e86aafdd01486f31a7a59f
littlepaike <f335125303@163.com>
2021-08-26 22:31:38 +0800

    2021/8/26

---
+++ 单文件组件/vue_test/src/components/School.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <!-- 组建的结构 -->
+        <div class="demo">
+            <h2>学校名称：{{schoolName}}</h2>
+            <h2>学校地址：{{address}}</h2>
+            <button @click="showName">点我提示学校名称</button>
+        </div>
+    </div>
+</template>
+
+
+<script>
+// 组件交互相关代码
+export default{
+    name:'School',
+    data(){
+        return{
+            schoolName:'aidpaike',
+            address:'beijing'
+        }
+    },
+    methods: {
+        showName(){
+            alert(this.schoolName)
+        }
+    },
+}
+
+</script>
+
+<style>
+    .demo{
+        background-color: orange;
+    }
+</style>
---
+++ 单文件组件/vue_test/src/components/Student.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <!-- 组建的结构 -->
+        <div class="demo">
+            <h2>学生名称：{{name}}</h2>
+            <h2>学生年龄：{{age}}</h2>
+        </div>
+    </div>
+</template>
+
+
+<script>
+// 组件交互相关代码
+export default{
+    name:'Student',
+    data(){
+        return{
+            name:'paike',
+            age:18
+        }
+    },
+}
+
+</script>
